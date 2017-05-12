9677d90643560729690ddd2569dff0495a49262f
psh-inter <psh.interhyp@gmail.com>
2017-05-12 13:37:23 +0200

    Generated with JHipster 4.3.0

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { Interhyp4HackathonSharedModule, UserRouteAccessService } from './shared';
+import { Interhyp4HackathonHomeModule } from './home/home.module';
+import { Interhyp4HackathonAdminModule } from './admin/admin.module';
+import { Interhyp4HackathonAccountModule } from './account/account.module';
+import { Interhyp4HackathonEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        Interhyp4HackathonSharedModule,
+        Interhyp4HackathonHomeModule,
+        Interhyp4HackathonAdminModule,
+        Interhyp4HackathonAccountModule,
+        Interhyp4HackathonEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class Interhyp4HackathonAppModule {}
---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,53 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    Interhyp4HackathonSharedLibsModule,
+    Interhyp4HackathonSharedCommonModule,
+    CSRFService,
+    AuthService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        Interhyp4HackathonSharedLibsModule,
+        Interhyp4HackathonSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        AuthService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        Interhyp4HackathonSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class Interhyp4HackathonSharedModule {}
