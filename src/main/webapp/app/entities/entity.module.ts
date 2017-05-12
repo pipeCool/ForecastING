2c9d048f0a58c0ef024e59add13aa07721164aeb
Reaper-XI <reaper-xi@gmx.de>
2017-05-12 22:11:14 +0200

    Calendar

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -5,6 +5,7 @@ import { Interhyp4HackathonOperationModule } from './operation/operation.module'
 import { Interhyp4HackathonBankAccountModule } from './bank-account/bank-account.module';
 import { Interhyp4HackathonTransactionMySuffixModule } from './transaction/transaction-my-suffix.module';
 import { Interhyp4HackathonLocationMySuffixModule } from './location/location-my-suffix.module';
+import { Interhyp4HackathonCalendarModule } from './calendar/calendar.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -14,6 +15,7 @@ import { Interhyp4HackathonLocationMySuffixModule } from './location/location-my
         Interhyp4HackathonBankAccountModule,
         Interhyp4HackathonTransactionMySuffixModule,
         Interhyp4HackathonLocationMySuffixModule,
+        Interhyp4HackathonCalendarModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
