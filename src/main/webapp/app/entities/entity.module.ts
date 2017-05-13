c7c8bb9b7f34f21801bd0b2b026738686fd151de
felipe.wieman@gmail.com <gh!58220>
2017-05-13 03:28:39 +0200

    it works

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -5,7 +5,6 @@ import { Interhyp4HackathonOperationModule } from './operation/operation.module'
 import { Interhyp4HackathonBankAccountModule } from './bank-account/bank-account.module';
 import { Interhyp4HackathonTransactionMySuffixModule } from './transaction/transaction-my-suffix.module';
 import { Interhyp4HackathonLocationMySuffixModule } from './location/location-my-suffix.module';
-import { Interhyp4HackathonCalendarModule } from './calendar/calendar.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -15,7 +14,6 @@ import { Interhyp4HackathonCalendarModule } from './calendar/calendar.module';
         Interhyp4HackathonBankAccountModule,
         Interhyp4HackathonTransactionMySuffixModule,
         Interhyp4HackathonLocationMySuffixModule,
-        Interhyp4HackathonCalendarModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
